3cf7557861d84f6431f169206fe0824ec1c1be0f
Chigoziem Iheanacho <chigoziemi@hotmail.com>
2022-09-14 08:43:20 +0100

    change

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -19,22 +19,22 @@ cat 2-largest_number.c
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+if (a > b && b > c)
+{
+largest = a;
+}
+else if (b > a && a > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
-    return (largest);
+return (largest);
 }
 cat main.h
 #ifndef MAIN_H
