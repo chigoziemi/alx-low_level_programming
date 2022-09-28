9e541ef9b69e7bd7f803d67bfd51ffd0f7119508
Chigoziem Iheanacho <chigoziemi@hotmail.com>
2022-09-28 11:36:00 +0100

    main

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt_check(int guess, int num);
+int is_prime_number(int n);
+int prime_check(int i, int num);
+int is_palindrome(char *s);
+int palindrome_check(int index, int revStrInd, char *str);
+
+#endif
